cf7de79e805859c20d1f5fd0bb3bcefc2dd86302
tomas <perezgantomas@gmail.com>
2019-03-20 23:15:33 -0400

    Project page - sliding Page funtionallity

--- JS/Main.js
+++ JS/Main.js
@@ -2,3 +2,10 @@ $(document).ready(function() {
   $('.HomePgMainBan_SubTX_1').fadeIn(1500).removeClass('hidden');
   $('.HomePgMainBan_SubTX_2').fadeIn(4500).removeClass('hidden');
 });
+
+$(document).ready(function() {
+  $( '.TheMainNav_Pro').on('click', function() {
+  $('.ProjectBox_Pg').toggle('slide',{direction:'right'}, 2000);
+  $('.ProjectBox_Pg').removeClass( 'hidden' );
+  });
+});
