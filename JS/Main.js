61430094e113db9554f038f3c6b6a16f8e137c24
tomas <perezgantomas@gmail.com>
2019-03-26 12:50:45 -0400

    project slider integration

--- JS/Main.js
+++ JS/Main.js
@@ -1,17 +1,31 @@
 $(document).ready(function() {
   $('.HomePgMainBan_SubTX_1').fadeIn(1500).removeClass('hidden');
-  $('.HomePgMainBan_SubTX_2').fadeIn(4500).removeClass('hidden');
+  $('.HomePgMainBan_SubTX_2').fadeIn(7500).removeClass('hidden');
 });
 
 $(document).ready(function() {
   $( '.TheMainNav_Pro').on('click', function() {
-  $('.ProjectBox_Pg').toggle('slide',{direction:'right'}, 2000);
+  $('.ProjectBox_Pg').toggle('slide').animate('slow');
   $('.ProjectBox_Pg').removeClass( 'hidden' );
+  $('.ProjectBox_Pg_TLT').fadeIn(3500).removeClass('hidden');
   });
 });
 $(document).ready(function() {
-  let ProjectLandpg = $('.ProjectBox_Pg_TXT')
+  var ProjectLandpg = $('.ProjectBox_Pg_TXT');
   $(ProjectLandpg).on('click', function() {
     $('.ProjectBox_Pg').hide();
   });
 });
+var Car_Home_1 = ["IMG/IMG_HOME_1.jpeg",
+                  "IMG/HOME_IMAGE_2.jpg"];
+var cnt = Car_Home_1.length;
+
+$(function() {
+        setInterval(Slider, 5000);
+    });
+
+    function Slider() {
+    $(".PRO_HOME_1_IMGS").show("fast", function() {
+       $(this).attr("src", Car_Home_1[(Car_Home_1.length++) % cnt]).show();
+    });
+    }
